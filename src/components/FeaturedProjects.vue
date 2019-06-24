8e311c0269cf891f6a4702b1b41250ee941a5477
Ninad <mohiteninad15@gmail.com>
2019-06-24 18:06:32 +0530

    Featured Projects alternating views implemented.

--- src/components/FeaturedProjects.vue
+++ src/components/FeaturedProjects.vue
@@ -2,7 +2,29 @@
 export default {
     props:{
         featuredProjects:Array
-    }
+    },
+   methods:{
+       styleObject: function (idx) {
+           return {
+                'text-align' : idx%2===1? 'start' : 'end' ,
+                'margin-left' : idx%2===1? '0px' : '-64px',
+                'margin-right' : idx%2===1? '-64px' : '0px',
+                'width' : '40%',
+                'z-index' : 1,
+                'background' : 'transparent',
+           }
+       },
+       listStyleObj(idx){
+           return{
+                'float' : idx%2===1? 'left' : 'right',
+                'margin-left' : idx%2===1? '0px' : '16px',
+                'margin-right' : idx%2===1? '16px' : '0px',
+                'color': 'white',
+                'opacity': 0.8, 
+
+           }
+       }
+   }
 }
 </script>
 
@@ -14,13 +36,13 @@ export default {
 
                 <v-layout row wrap justify-center> 
 
-                   <v-flex v-for="project in featuredProjects" 
-                        v-bind:key="project.name" xs12 sm10 md8 lg8 xl6 align-content-center 
-                        style="margin:16px;">
+                   <v-flex v-for="(project,idx) in featuredProjects" 
+                        v-bind:key="project.name" xs12 sm10 md8 lg8 xl8 align-content-center >
                        
                        <v-card color="transparent" flat >
 
-                           <v-parallax v-if=$vuetify.breakpoint.xsOnly :src="require('@/assets/demoSC.png')">
+                           <v-parallax v-if=$vuetify.breakpoint.xsOnly :src="require('@/assets/demoSC.png')" 
+                                style="margin:16px;">
 
                                 <v-layout align-center justify-end > 
                                    <div style="text-align:end; background:transparent;" >
@@ -44,37 +66,48 @@ export default {
                                 </v-layout>
                            </v-parallax>
 
-                           <v-layout v-else align-center >
+                           <v-layout v-else align-center style="margin-bottom: 124px; padding:20px">    
 
-                                <div style="float:left; width:60%;" >
+                                <v-flex style="width:60%;" :class="{'order-sm2' : idx%2===1}">
                                     <v-card flat>
                                         <v-img v-bind:src="require('@/assets/demoSC.png')" ></v-img>
                                     </v-card>
-                                </div>       
+                                </v-flex>                                   
 
-                                <div style="float:right;
-                                            text-align:end;
-                                            width:40%;
-                                            margin-left:-64px;
-                                            z-index:1;
-                                            background:transparent;" >
+                                <v-flex :style="styleObject(idx)" >
 
-                                    <span class="accent--text ">Featured Project</span><br/>
+                                    <span class="accent--text text-uppercase caption font-weight-bold" 
+                                        style="font-family: 'Monda', sans-serif;">Featured Project</span>
+                                    <br/>
 
-                                    <span class="headline font-weight-medium">{{project.name}}</span><br/><br/>
-
+                                    <span class="display-1 font-weight-bold white--text" style="opacity:0.9;">{{project.name}}</span>
+                                    <br/><br/>
+                                    
                                     <v-card color="secondary" tile dark >
                                         <v-card-text>
-                                            <span>{{project.description}}</span>
+                                            <span style="line-height:24px;">{{project.description}}</span>
                                         </v-card-text>
                                     </v-card>
 
                                     <br/>
                                     
                                     <ul>
-                                        <li v-for="tech in project.technologies" :key="tech">{{tech}}</li>
+                                        <li v-for="tech in project.technologies" :key="tech"
+                                            :style="listStyleObj(idx)">
+                                            {{tech}}
+                                        </li>
                                     </ul>
-                                </div>      
+                                    <br/>
+
+                                    <v-btn v-if="project.githubLink!==null" icon :href="project.githubLink" target="_blank">
+                                        <v-icon color="accent">fab fa-github</v-icon>
+                                    </v-btn>
+
+                                    <v-btn v-if="project.externalLink!==null" icon :href="project.externalLink" target="_blank">
+                                        <v-icon color="accent">open_in_new</v-icon>
+                                    </v-btn>
+                                    
+                                </v-flex>                                    
 
                             </v-layout>
                            
@@ -100,10 +133,5 @@ export default {
 
     ul li::before {
         content: none;  /* Add content: \2022 is the CSS Code/unicode for a bullet */
-    }
-
-    li {
-        float: right;
-        margin-left: 16px;
     }
 </style>
